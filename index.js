9bb7239a74755122c23130e7ef44c9c6f0874a46
rocher71 <qja086351@naver.com>
2023-09-20 16:28:27 -0700

    redux pattern comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//since it's vanilla js, 'require' is used.
 const redux = require('redux')
 const createStore = redux.createStore
 
@@ -20,7 +21,6 @@ function buyCake() {
 // reducer
 // (previousState, action) => newState
 // state has to be represented by a single object
-
 const initialState = {
   numOfCakes: 10
 }
@@ -37,10 +37,28 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = createStore(reducer);
+/*
+ redux store : brings actions and reducers together
+ one store for the entire application
+ 'getState()' : gives access to the state it holds.
+ 'dsipatch()' : updates to application state. accepts an actions as it's parameter.
+ 'subscribe()' : allows application to register listeners, accepts a function as parameter, 
+    executed any time the state in the redux store changes. 
+    can unsubscribe to the store by calling the function that was returned by subscribe().
+*/
+const store = createStore(reducer); //create redux store
+//expose a method that gives the current state in the store.
 console.log("initial state : ", store.getState());
+
+//allow app to subscribe to changes in the store that is achieved using the subscribe()
+//listener to the store, so anytime the store updates, this runs
 const unsubscribe = store.subscribe(() => console.log("updated state :", store.getState()));
+
+// dispatch accepts an actions as parameter. 
+// can directly provide the action, but use an action creator, which return the action.
+store.dispatch(buyCake()); //action of buying a cake
 store.dispatch(buyCake());
 store.dispatch(buyCake());
-store.dispatch(buyCake());
+
+// unsubscribe from the store by calling the function returned by the subscribe method.
 unsubscribe();
