c3360b7594cb89e339d2f1b6ae97a666fa38fe27
rocher71 <qja086351@naver.com>
2023-09-20 02:34:45 -0700

    create action

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("From index.js");
+const BUY_CAKE = 'BUY_CAKE';
+
+//define action : object that has a 'type' property
+// {
+//   type: BUY_CAKE
+//   info: 'First redux action'
+// }
+
+//action creator : function that returns an action
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action'
+  }
+}
