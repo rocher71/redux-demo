272a9b2851ebf5dafbeef9e7fe05e8df16900542
rocher71 <qja086351@naver.com>
2023-09-20 02:47:28 -0700

    store, subscribe

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
 const BUY_CAKE = 'BUY_CAKE';
 
 //define action : object that has a 'type' property
@@ -33,3 +36,11 @@ const reducer = (state = initialState, action) => {
     default: return state
   }
 }
+
+const store = createStore(reducer);
+console.log("initial state : ", store.getState());
+const unsubscribe = store.subscribe(() => console.log("updated state :", store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
